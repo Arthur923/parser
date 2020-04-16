39de1a6fea132fdcb1ec0668786017dd127a009c
Arthur923 <54887865+Arthur923@users.noreply.github.com>
2020-04-16 18:59:08 +0300

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,11 +1,10 @@
 window.addEventListener('DOMContentLoaded', () => {
     const body = document.querySelector('body');
     let textNode = [];
-    // body.childNodes.forEach(node => {
-    //     console.log(node);
-    // });
-    let list = [];
-
+     body.childNodes.forEach(node => {
+         console.log(node);
+    });
+    let mass = [];
     function rec (element){
         element.childNodes.forEach(node => {
             if (node.nodeName.match(/^L/)){
@@ -13,26 +12,19 @@ window.addEventListener('DOMContentLoaded', () => {
                     header: node.nodeName,
                     content: node.textContent
                 };
-                textNode.push(obj);
-                
+                textNode.push(obj);  
             } else{
                 rec(node);
-            };
-            
+            };       
         });
     }
-
     rec(body);
     console.log(textNode);
-
     function addTxt(arg){
         for(i=0; i<arg.length; i++){
             list.push(arg[i].content);
         }
     };
-
     addTxt(textNode);
-    console.log(list);
-
-
+    console.log(mass);
 });
